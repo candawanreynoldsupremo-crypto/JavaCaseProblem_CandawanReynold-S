2edda09b2b9b14fd2c5a31a7502784a985ccb9d8
candawanreynoldsupremo-crypto <candawanreynoldsupremo@gmail.com>
2025-10-16 13:15:29 +0800

    Add files via upload

---
+++ Candawan_R,2C/JavaCaseProblem_Ren.java
@@ -0,0 +1,17 @@
+public class JavaCaseProblem_Ren{
+    public static void main(String[] args) {
+        String fullName = "Reynold S. Candawan";
+        String schoolID = "2024304143";
+
+        
+        double celsius = 36.5;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        
+        System.out.println("Name: " + fullName);
+        System.out.println("ID Number: " + schoolID);
+        System.out.println("------------------------------");
+        System.out.printf("Result: %.1f°C is equivalent to %.1f°F%n", celsius, fahrenheit);
+        
+    }
+}
